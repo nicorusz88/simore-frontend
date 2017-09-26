e32e44405cc08cdad31d539352d0812c54fd363d
Maximiliano Garcia <magf.1987@gmail.com>
2017-09-25 23:52:02 -0300

    WIP - Treatment datils with all widgets

--- src/app/pages/patients/treatment/PatientsTreatmentCtrl.js
+++ src/app/pages/patients/treatment/PatientsTreatmentCtrl.js
@@ -9,54 +9,19 @@
     .controller('PatientsTreatmentCtrl', PatientsTreatmentCtrl);
 
   /** @ngInject */
-  function PatientsTreatmentCtrl($scope, $state, User, TreatmentTemplate) {
+  function PatientsTreatmentCtrl($scope, $stateParams, $state, User, TreatmentTemplate) {
     var vm = this;
-    vm.entry = new User({ roles: [{ id: 2, name: 'PATIENT' }] });
+    vm.entry = undefined;
 
-    vm.save = save;
-    vm.arePersonalInfoPasswordsEqual = arePersonalInfoPasswordsEqual;
 
-    vm.treatmentsTemplates = [];
-    vm.proffesionals = [];
-
-    TreatmentTemplate.query({}, function(data) {
-      vm.treatmentsTemplates = data;
+    User.get( { userId: $stateParams.patientId}, function(user) {
+      vm.entry = user;
     });
 
-    User.query({ cmd: 'roles', roles: 'PROFESSIONAL' }, function(data) {
-      vm.proffesionals = data;
-    });
-
-    vm.dt = new Date();
-    vm.options = {
-      showWeeks: false
-    };
-
-    vm.open = open;
-    vm.opened = false;
-    vm.options = {
-      showWeeks: false
-    };
-
+ 
     ////////////////////
 
-    function arePersonalInfoPasswordsEqual() {
-      return vm.entry.confirmPassword && vm.entry.password == vm.entry.confirmPassword;
-    }
-
-    function open() {
-      vm.opened = true;
-    }
-
-    function save(form) {
-      if (form.$valid) {
-        vm.error = {};
-
-        vm.entry.$save({}, function() {
-          $state.go('patients.index', null, { reload: true });
-        });
-      }
-    }
+    
   }
 
 })();
