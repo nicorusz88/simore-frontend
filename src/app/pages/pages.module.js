209986e5f2f8ce4394923c4aa421420e3638ec1b
Maximiliano Garcia <magf.1987@gmail.com>
2017-12-21 00:34:48 -0300

    Saco los componentes que agregué

--- src/app/pages/pages.module.js
+++ src/app/pages/pages.module.js
@@ -11,7 +11,6 @@
     'SimoreFrontend.pages.users',
     'SimoreFrontend.pages.treatments',
     'SimoreFrontend.pages.patients',
-    'SimoreFrontend.pages.components',
     //'SimoreFrontend.pages.tables',
     //'SimoreFrontend.pages.charts',
     //'SimoreFrontend.pages.maps',
